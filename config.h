01a0421ee6a3a6fa2cac94aedc27416af876ad22
fwxzxh <jdemeca01@gmail.com>
2023-10-15 12:04:57 -0600

    added keyboard config

---
+++ config.h
@@ -0,0 +1,13 @@
+#pragma once
+
+/* The way how "handedness" is decided (which half is which),
+see https://docs.qmk.fm/#/feature_split_keyboard?id=setting-handedness
+for more options.
+*/
+
+// Configure the global tapping term (default: 200ms)
+#undef TAPPING_TERM
+#define TAPPING_TERM 170
+
+// Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
+#define QUICK_TAP_TERM 120
