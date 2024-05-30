088bb073d8112076ccffcc49441ae9875becfaac
fwxzxh <jdemeca01@gmail.com>
2024-05-30 00:55:01 -0600

    Fixed Ghost iayout bug xd

--- config.h
+++ config.h
@@ -7,7 +7,7 @@ for more options.
 
 // Configure the global tapping term (default: 200ms)
 #undef TAPPING_TERM
-#define TAPPING_TERM 230
+#define TAPPING_TERM 260
 
 // Enable rapid switch from tap to hold, disables double tap hold auto-repeat.
 #define QUICK_TAP_TERM 120
